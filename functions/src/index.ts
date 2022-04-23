b0caa81591f6ae9563713d98810f7af9e8e041b8
Steve Lorentzen <Stevelorentzen@gmail.com>
2022-04-23 00:03:31 -0700

    set up cloud functions wrote function to create student document when a user signs in for the first time

---
+++ functions/src/index.ts
@@ -0,0 +1,20 @@
+// import * as functions from 'firebase-functions'
+
+import * as admin from 'firebase-admin'
+admin.initializeApp()
+
+// exports.createUserDoc = functions.auth.user().onCreate(user => {
+//   console.log(user)
+//   admin
+//     .firestore()
+//     .collection('students')
+//     .doc(user.uid)
+//     .set({
+//       email: user.email,
+//       displayName: user.displayName,
+//       photoURL: user.photoURL,
+//       uid: user.uid,
+//     })
+//     .then(res => console.log(res))
+//     .catch(err => console.log(err))
+// })
